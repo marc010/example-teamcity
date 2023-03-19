52b10256b1ac454799ced6c683349ee8634d0535
Ilya T <i.tokarev@me.com>
2023-03-19 20:50:33 +0300

    added new method in class Welcomer

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHunter(){
+		return "Hi! I return a world hunter, twice: hanter, hanter";
+	}
 }
